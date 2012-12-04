04b2bb847c4753aeba79977c85f772e4dc394ecd
Przemyslaw Peron <huan@huan.pl>
2012-12-04 08:14:34 +0100

    $1

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-whmcs-dotpay
-============
+DotPay.pl Payment Gateway dla WHMCS v1.0
+Autor: Przemek Peron <huan@huan.pl>
+Website: http://www.huan.pl
+
+----------------------------------------
+
+Skrypt jest rozpowszechniany na licencji Creative Commons Attribution 3.0
+Brzmienie licencji: http://creativecommons.org/licenses/by/3.0/pl/
+
+
+INSTRUKCJA INSTALACJI:
+----------------------
+
+1. Wgranie plikow na serwer
+
+Kopiujemy pliki do instalacji WHMCS do katalogu /modules/gateways:
+
+dotpay.php --> /modules/gateways/dotpay.php
+callback/dotpay.php --> /modules/gateways/callback/dotpay.php
+
+2. Aktywacja modulu w WHMCS
+
+Nalezy zalogowac sie do administracji WHMCS i w dziale "Setup / Payment Gateways",
+wybrac gateway "DotPay.pl" i aktywowac go.
+
+Nastepnie w konfiguracji modulu DotPay.pl wystarczy juz tylko wprowadzic swoj login
+z serwisu DotPay w polu "Login ID"
+
+PAMIETAJ: nalezy zalogowac sie do serwisu dotpay.pl i w Ustawieniach w opcji
+"Parametry URLC" aktywowac opcje "Zezwol na przyjecie parametru URLC z zewnatrz"
+
+3. Gotowe
+
+
+DODATKOWE INFORMACJE
+--------------------
+
+
+Skrypt zostal napisany z mysla o polskich uzytkownikach, wiec nie umieszczalem
+w nim obslugi innych walut. Testowany byl z WHMCS v4.0 - v4.3, a czy bedzie dzialac
+na wersji, ktora wlasnie posiadasz? Bardzo mozliwe, ale nie daje zadnych gwarancji ;)
+
---
+++ dotpay.php
@@ -0,0 +1,64 @@
+<?php
+
+function dotpay_config() {
+    $configarray = array(
+     "FriendlyName" => array("Type" => "System", "Value"=>"DotPay.pl"),
+     "username" => array("FriendlyName" => "Login ID", "Type" => "text", "Size" => "20", ),
+    );
+	return $configarray;
+}
+
+function dotpay_link($params) {
+
+	# Gateway Specific Variables
+	$gatewayusername = $params['username'];
+	$gatewaytestmode = $params['testmode'];
+
+	# Invoice Variables
+	$invoiceid = $params['invoiceid'];
+	$description = $params["description"];
+    $amount = $params['amount']; # Format: ##.##
+    $currency = $params['currency']; # Currency Code
+
+	# Client Variables
+	$firstname = $params['clientdetails']['firstname'];
+	$lastname = $params['clientdetails']['lastname'];
+	$email = $params['clientdetails']['email'];
+	$address1 = $params['clientdetails']['address1'];
+	$address2 = $params['clientdetails']['address2'];
+	$city = $params['clientdetails']['city'];
+	$state = $params['clientdetails']['state'];
+	$postcode = $params['clientdetails']['postcode'];
+	$country = $params['clientdetails']['country'];
+	$phone = $params['clientdetails']['phone'];
+
+	# System Variables
+	$companyname = $params['companyname'];
+	$systemurl = $params['systemurl'];
+	$currency = $params['currency'];
+
+	# Enter your code submit to the gateway...
+
+	$code = '<form method="POST" action="https://ssl.dotpay.pl">
+<input type="hidden" name="id" value="'.$gatewayusername.'" />
+<input type="hidden" name="firstname" value="'.$firstname.'" />
+<input type="hidden" name="lastname" value="'.$lastname.'" />
+<input type="hidden" name="street" value="'.$address1.'" />
+<input type="hidden" name="email" value="'.$email.'" />
+<input type="hidden" name="city" value="'.$city.'" />
+<input type="hidden" name="postcode" value="'.$postcode.'" />
+<input type="hidden" name="phone" value="'.$phone.'" />
+<input type="hidden" name="control" value="'.$invoiceid.'" />
+<input type="hidden" name="description" value="'.$invoiceid.' '.$description.'" />
+<input type="hidden" name="amount" value="'.$amount.'" />
+<input type="hidden" name="URL" value="'.$params['systemurl'].'/viewinvoice.php?id='.$invoiceid.'">
+<input type="hidden" name="URLC" value="'.$params['systemurl'].'/modules/gateways/callback/dotpay.php">
+<input type="submit" value="Zaplac teraz" />
+</form>';
+
+	return $code;
+}
+
+
+
+?>
